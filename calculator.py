1646244d18846d950c24f194e6dfc3db6d9d1869
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2023-11-05 22:08:23 -0500

    using finance terms

--- calculator.py
+++ calculator.py
@@ -1,17 +1,13 @@
 from config import *  
 
-incomeTaxesToPay = effectiveTaxRate = 0
-taxableIncome = TAXABLE_INCOME #removing standard deduction
+taxLiability = effectiveTaxRate = 0
+taxableIncome = TAXABLE_INCOME
 
-#fica tax
-if  taxableIncome - FICA_TAX_INCOME > 0:
-    incomeTaxesToPay = FICA_TAX_INCOME * FICA_TAX
+#fica tax, flat rate up to income limit
+if  taxableIncome - FICA_TAX_INCOME > 0: #if income exceed this limit, then we just tax at the limit
+    taxLiability = FICA_TAX_INCOME * FICA_TAX
 else:
-    print(taxableIncome)
-    incomeTaxesToPay = taxableIncome * FICA_TAX
-    
-print(incomeTaxesToPay)
-
+    taxLiability = taxableIncome * FICA_TAX #else tax the amount below the limit
 
 
 #income tax
@@ -20,18 +16,19 @@ for index in range(1,len(FED_TAX_INCOME_BUCKETS)): # go through each tax bracket
     taxableIncome = taxableIncome - (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) #remove the money from each bucket, moving up the buckets
     
     if taxableIncome < 0: #if at any point the remaining taxable balance is negative, this means we've reached the highest bucket. Undo this and then tax the remaining amount
-        incomeTaxesToPay +=  (taxableIncome + (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1])) * (FED_TAX[index-1])
+        taxLiability +=  (taxableIncome + (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1])) * (FED_TAX[index-1])
         taxableIncome = 0  
         break
     
-    incomeTaxesToPay +=  (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) * (FED_TAX[index-1]) #else add the tax from this bucket and move to the next one
+    taxLiability +=  (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) * (FED_TAX[index-1]) #else add the tax from this bucket and move to the next one
 
 
 
 
 if ADJUSTED_GROSS_INCOME > 0:
-    effectiveTaxRate = (incomeTaxesToPay / ADJUSTED_GROSS_INCOME) * 100
-    print("You have an effective tax rate of {:.2f}% and have to pay ${:,.2f} in taxes".format(effectiveTaxRate, incomeTaxesToPay))   
+    effectiveTaxRate = (taxLiability / ADJUSTED_GROSS_INCOME) * 100
+    print("You have an effective tax rate of {:.2f}% and have to pay ${:,.2f} in taxes".format(effectiveTaxRate, taxLiability))   
+    print()
 else:
     print("You have no taxes to pay")
 
