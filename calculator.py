900d5c5f2479298ea6379daaa778571a5bd09b74
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2023-11-05 20:28:15 -0500

    pension  and fica

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 from config import *  
 
 incomeTaxesToPay = effectiveTaxRate = 0
-taxableIncome = W2_INCOME - STANDARD_DEDUCTION #removing standard deduction
+taxableIncome = TAXABLE_INCOME #removing standard deduction
 
 
 for index in range(1,len(FED_TAX_INCOME_BUCKETS)): # go through each tax bracket
@@ -9,16 +9,13 @@ for index in range(1,len(FED_TAX_INCOME_BUCKETS)): # go through each tax bracket
     taxableIncome = taxableIncome - (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) #remove the money from each bucket, moving up the buckets
     
     if taxableIncome < 0: #if at any point the remaining taxable balance is negative, this means we've reached the highest bucket. Undo this and then tax the remaining amount
-        incomeTaxesToPay +=  (taxableIncome + (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1])) * (FED_TAX[index-1] / 100)
+        incomeTaxesToPay +=  (taxableIncome + (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1])) * (FED_TAX[index-1])
         taxableIncome = 0  
         break
     
-    incomeTaxesToPay +=  (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) * (FED_TAX[index-1] / 100) #else add the tax from this bucket and move to the next one
-    print(taxableIncome)
-    print(incomeTaxesToPay)
-    print("-------------")
+    incomeTaxesToPay +=  (FED_TAX_INCOME_BUCKETS[index] - FED_TAX_INCOME_BUCKETS[index - 1]) * (FED_TAX[index-1]) #else add the tax from this bucket and move to the next one
+   
 
-    
     
 
 effectiveTaxRate = (incomeTaxesToPay / W2_INCOME) * 100
--- config.py
+++ config.py
@@ -7,34 +7,52 @@ Married filing jointly = 2
 Married filing separately = 3
 Head of household = 4
 """
-FILING_STATUS = 1
+filingStatus = 1
 
-# ALL INCOME STREAMS BELOW
+# ALL YEARLY INCOME STREAMS BELOW
 
 W2_INCOME = 100000
-# FERS Pension calculator
-yearsOfService = 0
-pensionPercent = .01
-high3 = 0
-PENSION_INCOME = yearsOfService * pensionPercent * high3
 
-SOCIAL_SECURITY_INCOME = 0
+# Do you have a pension? Yes(1) or no(0)?
+pensionStatus = 0
+
+if pensionStatus == 1:
+    # FERS Pension calculator
+    yearsOfService = 30
+    pensionPercent = .01 # .01 or .011
+    high3 = 70000
+    PENSION_INCOME = yearsOfService * (pensionPercent) * high3
+else:
+    PENSION_INCOME = 0
+
+
+
+SOCIAL_SECURITY_INCOME = 2000 * 12
+
+
+MARKET_RATE_OF_RETURN_PERCENT = .06 
 EMPLOYER_MATCH_INCOME = 0
 IRA_INCOME = 0
 HEALTH_PLAN_INCOME = 0
-ROTH_INCOME = 100000
 CAP_GAINS = 0
+
+
+ROTH_INCOME = 100000
+
 
 
 
 # ALL THE TAX CALCULATIONS ARE BELOW
-# Tax rates, added a 0 for the first element 
-FED_TAX = [10, 12, 22, 24, 32, 35, 37]
-CAP_GAINS_TAX = [0, 15, 20]
-SS_TAX = [0, 50, 85]
+# Tax rates
+FED_TAX = [.10, .12, .22, .24, .32, .35, .37]
+CAP_GAINS_TAX = [.0, .15, .20]
+SS_TAX = [.0, .50, .85]
+FICA_TAX = .0765 #ignoring medicare tax after 200k
+
 
 # Federal tax rates and standard deduction based on filing status, added 0 to start of tax buckets so subtraction for first bucket would not break code
-match FILING_STATUS:
+FICA_TAX_INCOME = 160200
+match filingStatus:
     case 1:
         FED_TAX_INCOME_BUCKETS = [0, 11000, 44725, 95375, 182100, 231250, 578125, 999999999]
         CAP_GAINS_TAX_INCOME_BUCKETS  = [44625, 492300, 999999999]
@@ -59,10 +77,17 @@ match FILING_STATUS:
         print("Not valid filing status")
         FED_TAX_INCOME_BUCKETS = None
         CAP_GAINS_TAX_INCOME_BUCKETS = None
+        SS_TAX_INCOME_BUCKETS = None
         STANDARD_DEDUCTION = None
 
-NONTAXABLE_INTEREST = W2_INCOME + PENSION_INCOME + EMPLOYER_MATCH_INCOME + IRA_INCOME + HEALTH_PLAN_INCOME
-TAX_LIABILITY = NONTAXABLE_INTEREST - STANDARD_DEDUCTION
+
+NONTAXABLE_INTEREST = W2_INCOME + EMPLOYER_MATCH_INCOME + IRA_INCOME + HEALTH_PLAN_INCOME
+# TAX_LIABILITY = NONTAXABLE_INTEREST - STANDARD_DEDUCTION
+
+#just testing w2 rn
+ADJUSTED_GROSS_INCOME = W2_INCOME + PENSION_INCOME
+TAXABLE_INCOME = ADJUSTED_GROSS_INCOME - STANDARD_DEDUCTION
+
 
 #RMD for Traditional Accounts, starts at 72
 RMD_DIVISOR = [27.4, 26.5, 25.5, 24.6, 23.7, 22.9, 22, 21.1, 20.2, 19.4, 18.5, 17.7, 16.8, 16, 15.2, 14.4, 13.7, 12.9, 12.2, 11.5, 10.8, 10.1, 9.5, 8.9, 8.4, 7.8, 7.3, 6.8, 6.4, 6, 5.6, 5.2, 4.9, 4.6, 4.3, 4.1, 3.9, 3.7, 3.5, 3.4, 3.3, 3.1, 3, 2.9, 2.8, 2.7, 2.5, 2.3, 2]
